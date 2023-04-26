d75ede09fb6c1b6ce863eef2d541c0ea50e03f79
diogeneslimax <diogenes.lima@uctechnology.com.br>
2023-04-26 16:55:07 -0300

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const express = require('express');
+const app = express();
+
+// Função de validação de CPF (https://www.macoratti.net/alg_cpf.htm)
+
+// A validação do Primeiro Digito multiplicamos cada um dos números, da direita para a esquerda por números crescentes a partir do número 2
 function validarPrimeiroDigito(cpf) {
   let sum = 0;
   for (let i = 0; i < 9; i++) {
@@ -9,7 +15,7 @@ function validarPrimeiroDigito(cpf) {
   }
   return cpf[9] == 0;
 }
-
+// A Validação do Segundo Digito 
 function validarSegundoDigito(cpf) {
   let sum = 0;
   for (let i = 0; i < 10; i++) {
@@ -22,6 +28,7 @@ function validarSegundoDigito(cpf) {
   return cpf[10] == 0;
 }
 
+
 function validarRepetido(cpf) {
   const primeiro = cpf[0];
   let diferente = false;
@@ -32,25 +39,40 @@ function validarRepetido(cpf) {
   }
   return diferente;
 }
-
+// Se o numero de gitos for maior que 11
 function validarCpf(cpf) {
   if (cpf.length != 11) {
     return false;
   }
+//   Se for repetido
   if(!validarRepetido(cpf)) {
     return false;
   }
+//   Se regra do primeiro digito não for verdadeira
   if (!validarPrimeiroDigito(cpf)) {
     return false;
   }
+//   Se regra do Segundo digito não for verdadeira
   if (!validarSegundoDigito(cpf)) {
     return false;
   }
   return true;
 }
 
+
 const cpf = "37873528804".split("").map((e) => parseInt(e));
 
 const cpfValido = validarCpf(cpf);
 
-console.log(cpfValido);
+// console.log(cpfValido);
+
+app.get('/validar-cpf/:cpf', (req, res) => {
+  const cpf = req.params.cpf.split("").map((e) => parseInt(e));
+  const cpfValido = validarCpf(cpf);
+  res.json({ cpfValido });
+});
+
+app.listen(3009, () => {
+  console.log('Servidor iniciado na porta 3000');
+});
+
